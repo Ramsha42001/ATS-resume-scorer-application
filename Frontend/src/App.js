8dbd665b3305c429cf4121f8eb84abb7295a3fe3
Pranjal Mishra <prjrules07@gmail.com>
2024-04-16 15:14:37 +0530

    Merged Login and Register

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,43 @@
-import React from 'react'
-import Home from './Pages/Home/Home';
-import Navbar from './Pages/Navbar/Navbar';
-import About from './Pages/About/About'
-
+import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from './Pages/LoginPage/LoginPage';
+import axios from 'axios';
+import RegisterPage from '../src/Pages/RegisterPage/RegisterPage';
+import {Toaster} from 'react-hot-toast'
+import { UserContextProvider } from './context/userContext';
+import HomePage from '../src/Pages/Home/Home';
+import Navbar from './Pages/Navbar/Navbar'
+import AboutPage from './Pages/About/About'
+axios.defaults.baseURL ='http://localhost:8000'
+axios.defaults.withCredentials=true;
 function App() {
-  return (
-   <>
-   <Navbar />
-   <Home />
-   <About/>
-   </>
+  return ( 
+    <UserContextProvider >
+    <BrowserRouter>
+    <Navbar />
+    <Toaster  position="top-center" toastOptions={{duration:2000}} />
+      <Routes>
+      <Route
+          path="/"
+          element={<div><HomePage /></div>}
+        />
+        <Route
+          path="/about"
+          element={<div><AboutPage /></div>}
+        />
+        <Route
+          path="/login"
+          element={<div><LoginPage /></div>}
+        />
+        {/* Define other routes here */}
+        <Route
+          path="/register"
+          element={<div><RegisterPage /></div>}
+        />
+      </Routes>
+    </BrowserRouter>
+    </UserContextProvider>
   );
 }
 
--- Frontend/src/Pages/Navbar/Navbar.js
+++ Frontend/src/Pages/Navbar/Navbar.js
@@ -1,21 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link from React Router
 import NavCss from './Navbar.module.css';
 import logo from './logo.jpg';
-function Navbar()
-{
-    return (
-     <>
-    <div className={NavCss.mainBox}>
-      <div className={NavCss.logoDiv}>
-        <img className={NavCss.logo} src={logo}/>
+
+function Navbar() {
+  return (
+    <>
+      <div className={NavCss.mainBox}>
+        <div className={NavCss.logoDiv}>
+          <img className={NavCss.logo} src={logo} alt="Logo" />
+        </div>
+        <div className={NavCss.navDiv}>
+          <h2><Link to="/about">About</Link></h2>
+          {/* Use Link to navigate to the '/login' route */}
+          <h2><Link to="/login">Login</Link></h2>
+        </div>
       </div>
-      <div className={NavCss.navDiv}>
-        <h2>Home</h2>
-        <h2>Login</h2>
-      </div>
-       </div>
-     </>
-    );
+    </>
+  );
 }
 
 export default Navbar;
