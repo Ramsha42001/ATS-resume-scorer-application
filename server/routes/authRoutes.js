dde7938ec578d241b62c0c9b64529ba88927f357
Pranjal Mishra <prjrules07@gmail.com>
2024-04-16 16:06:43 +0530

    Updated CORS config

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const {test,registerUser,loginUser, getProfile} = require('../controllers/authCo
 router.use(
     cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: 'https://ats-resume-scorer-application.vercel.app/',
     })
 );
 
