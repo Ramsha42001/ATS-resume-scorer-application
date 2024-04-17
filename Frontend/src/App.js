bd073e60c00ab0545c2b5268f06652c1d5efeaad
Ramsha42001 <90575580+Ramsha42001@users.noreply.github.com>
2024-04-17 14:31:03 +0530

    update app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -14,27 +14,14 @@ axios.defaults.baseURL ='https://ats-resume-scorer-application-2.onrender.com';
 axios.defaults.withCredentials = true;
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    axios.get("/api/checkAuth")
-      .then(response => {
-        setIsLoggedIn(true); 
-        console.log("logged in")
-      })
-      .catch(error => {
-        setIsLoggedIn(false);
-        console.log("not logged in")
-      });
-  }, []);
 
   return (
     <BrowserRouter>
-    {  isLoggedIn? <Navbar />: ''}
+     <Navbar />
       <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
       <Routes>
-        <Route path="/" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/about" element={<AboutPage />} />
